1439be1dd3030bea2eb08b2db6bdcc81df01cb59
abhi64230 <abhisekpradhan615@gmail.com>
2024-08-02 18:36:40 +0530

    Completed

---
+++ Day 11/Jump Game.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int targetNumIndex = nums.size() - 1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (targetNumIndex <= i + nums[i]) {
+                targetNumIndex = i;
+            }
+        }
+        return targetNumIndex == 0; 
+    }
+};
